d491178e928812119adf16fca39534945e71cc17
davidgeorgebell <bell.dave93@gmail.com>
2020-06-10 11:03:25 +0800

    images fetched and displayed

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,43 @@
 import Head from 'next/head';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+
+// export async function getStaticProps() {
+//   const res = await fetch(`${baseUrl}${accessKey}`);
+//   const images = await res.json();
+//   console.log(images);
+//   return {
+//     props: {
+//       images,
+//     },
+//   };
+// }
+
+// function ImageList({ images }) {
+//   return (
+//     images &&
+//     images.map((image, index) => (
+//       <div key={index}>
+//         <img src={image.urls.regular} alt={image.alt_description} />
+//       </div>
+//     ))
+//   );
+// }
 
 const baseUrl = 'https://api.unsplash.com/photos/?client_id=';
-const accessKey = process.env.ACCESS_KEY;
+const accessKey = process.env.NEXT_PUBLIC_ACCESS_KEY;
 
-export async function getStaticProps() {
-  const res = await fetch(`${baseUrl}${accessKey}`);
-  const images = await res.json();
-  console.log(images);
-  return {
-    props: {
-      images,
-    },
-  };
-}
+export default function Home() {
+  const [images, setImages] = useState([]);
 
-export default function Home({ images }) {
+  useEffect(() => {
+    async function getImages() {
+      const res = await fetch(`${baseUrl}${accessKey}`);
+      const data = await res.json();
+      setImages(data);
+    }
+    getImages();
+  }, []);
+
   return (
     <div className='container'>
       <Head>
@@ -26,6 +48,13 @@ export default function Home({ images }) {
       <main>
         <h1>Infinite scroll</h1>
         <input type='text' placeholder='search' />
+        <div className='image-grid'>
+          {images.map((image, index) => (
+            <div className='image' key={index}>
+              <img src={image.urls.regular} alt={image.alt_description} />
+            </div>
+          ))}
+        </div>
       </main>
 
       <footer>David Bell</footer>
