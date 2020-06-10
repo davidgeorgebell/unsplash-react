565616fad6ab0122e67f41b54176c6219bdc3e5a
davidgeorgebell <bell.dave93@gmail.com>
2020-06-10 09:47:05 +0800

    image data fetched statically

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,31 @@
 import Head from 'next/head';
+import React, { useEffect } from 'react';
 
-export default function Home() {
+const baseUrl = 'https://api.unsplash.com/photos/?client_id=';
+const accessKey = process.env.ACCESS_KEY;
+
+export async function getStaticProps() {
+  const res = await fetch(`${baseUrl}${accessKey}`);
+  const images = await res.json();
+  console.log(images);
+  return {
+    props: {
+      images,
+    },
+  };
+}
+
+export default function Home({ images }) {
   return (
     <div className='container'>
       <Head>
-        <title>Create Next App</title>
+        <title>React Infinite Scroll</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <H1>Infinite scroll</H1>
+        <h1>Infinite scroll</h1>
+        <input type='text' placeholder='search' />
       </main>
 
       <footer>David Bell</footer>
